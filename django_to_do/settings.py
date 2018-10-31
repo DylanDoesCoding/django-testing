fcce3314d39b229e3d8097218e34a9b99ded7a69
DylanCahill <dylancahill.ireland@gmail.com>
2018-10-31 13:27:19 +0000

    Prep settings and requirments for deployment

--- django_to_do/settings.py
+++ django_to_do/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,13 +75,13 @@ WSGI_APPLICATION = 'django_to_do.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+DATABASES = {'default' : dj_database_url.parse("postgres://inumgcafhvycue:979e82f667ae77eaff82ab3e1e8fff1eba830b4bdfc98288681312ec3ca59aad@ec2-54-247-86-89.eu-west-1.compute.amazonaws.com:5432/d4f3j0g2jq9o6i")}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
